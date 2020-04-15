fef7db854e072b6677a2815f68c6ccc7607e91bd
Jennifer Groetz <ogroetz@gmail.com>
2020-04-15 03:36:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-
+  sounds = ["talk", "whisper", "yell", "bark"]
+  sounds << "hum"
 end
 
 def add_element_to_start_of_array(array, element)
